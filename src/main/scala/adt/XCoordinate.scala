aed65d9ee5b8411979ebf8754f429c53986d22b4
Ashwin Bhaskar <ashwinbhskr@gmail.com>
2020-09-06 09:26:15 +0530

    Add extension ops

--- src/main/scala/adt/XCoordinate.scala
+++ src/main/scala/adt/XCoordinate.scala
@@ -5,8 +5,29 @@ import Error._
 opaque type XCoordinate = Int
 
 object XCoordinate:
+    private[adt] val error = InvalidXCoordinate("Value should be less than 10 and greater or equal to 0")
     def apply(x: Int): Either[Error, XCoordinate] = 
         if(x >= 0 && x < 10)
             Right(x)
         else
             Left(InvalidXCoordinate("Value should be less than 10 and greater or equal to 0"))
+
+    def isValid(y: Int): Boolean = y >= 0 && y < 10
+
+    def force(x: Int): XCoordinate = 
+        if(isValid(x))
+            x
+        else
+            throw new IllegalArgumentException("Value should be less in the range 0 to 10")
+
+extension XCoordinateOps on (x: XCoordinate):
+    def +(o: Int): Either[Error, XCoordinate] = 
+        if(XCoordinate.isValid(o + x))
+            Right(o + x)
+        else
+            Left(XCoordinate.error)
+    def -(o: Int): Either[Error, XCoordinate] = 
+        if(YCoordinate.isValid(x - o))
+            Right(x - o)
+        else
+            Left(XCoordinate.error)
--- src/main/scala/adt/YCoordinate.scala
+++ src/main/scala/adt/YCoordinate.scala
@@ -5,8 +5,29 @@ import Error._
 opaque type YCoordinate = Int
 
 object YCoordinate:
+    private[adt] val error = InvalidYCoordinate("Value should be less than 10 and greater or equal to 0")
     def apply(y: Int): Either[Error, YCoordinate] = 
-        if(y >= 0 && y < 10)
+        if(isValid(y))
             Right(y)
         else
-            Left(InvalidXCoordinate("Value should be less than 10 and greater or equal to 0"))
+            Left(error)
+    
+    def isValid(y: Int): Boolean = y >= 0 && y < 10
+
+    def force(y: Int): YCoordinate = 
+        if(isValid(y))
+            y
+        else
+            throw new IllegalArgumentException("Value should be in the range of 0 and 10")
+
+extension YCoordinateOps on (y: YCoordinate):
+    def +(o: Int): Either[Error, YCoordinate] = 
+        if(YCoordinate.isValid(o + y))
+            Right(o + y)
+        else
+            Left(YCoordinate.error)
+    def -(o: Int): Either[Error, YCoordinate] = 
+        if(YCoordinate.isValid(y - o))
+            Right(y - 0)
+        else
+            Left(YCoordinate.error)
