fbca047cf983cc51c265dad3762f163a3bd251be
Ashwin Bhaskar <ashwinbhskr@gmail.com>
2020-09-06 19:55:02 +0530

    Run scalafmt

--- src/test/scala/LocationTest.scala
+++ src/test/scala/LocationTest.scala
@@ -1,34 +1,34 @@
 import org.junit.Test
 import org.junit.Assert._
-import model.Location
+import model.Cell
 import adt.{YCoordinate, XCoordinate}
 
-class LocationTest {
+class CellTest {
   @Test
-  def nextPossibleLocationsTest = {
+  def nextPossibleCellsTest = {
     val startX = XCoordinate(2)
     val startY = YCoordinate(6)
-    val startLocation = for {
+    val startCell = for {
       x <- startX
       y <- startY
-    } yield (Location(x, y))
+    } yield (Cell(x, y))
 
-    startLocation match {
-      case Right(location) =>
+    startCell match {
+      case Right(cell) =>
         val actual =
-          Location.nextPossibleLocations(location).toSet
-        val east = location.copy(x = XCoordinate.force(5))
-        val north = location.copy(y = YCoordinate.force(9))
-        val south = location.copy(y = YCoordinate.force(3))
-        val northEast = Location(XCoordinate.force(4), YCoordinate.force(8))
-        val southEast = Location(XCoordinate.force(4), YCoordinate.force(4))
-        val northWest = Location(XCoordinate.force(0), YCoordinate.force(8))
-        val southWest = Location(XCoordinate.force(0), YCoordinate.force(4))
+          Cell.nextPossibleCells(cell).toSet
+        val east = cell.copy(x = XCoordinate.force(5))
+        val north = cell.copy(y = YCoordinate.force(9))
+        val south = cell.copy(y = YCoordinate.force(3))
+        val northEast = Cell(XCoordinate.force(4), YCoordinate.force(8))
+        val southEast = Cell(XCoordinate.force(4), YCoordinate.force(4))
+        val northWest = Cell(XCoordinate.force(0), YCoordinate.force(8))
+        val southWest = Cell(XCoordinate.force(0), YCoordinate.force(4))
         val expected =
           Set(east, north, south, northEast, southEast, northWest, southWest)
 
         assertEquals(expected, actual)
-      case Left(_) => assertFail("Should have been a valid location")
+      case Left(_) => assertFail("Should have been a valid Cell")
     }
   }
 }
